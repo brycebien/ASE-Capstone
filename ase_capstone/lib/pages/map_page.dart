f17e6ed225e8baa266e4b48647a1088527e3fbd3
brycebien <brycebien@gmail.com>
2025-01-20 16:41:33 -0500

    app opens centered on NKU's campus

--- ase_capstone/lib/pages/map_page.dart
+++ ase_capstone/lib/pages/map_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key});
@@ -8,8 +9,17 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
+  static const LatLng _center = LatLng(39.033, -84.4631);
+
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    return Scaffold(
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: _center,
+          zoom: 16.0,
+        ),
+      ),
+    );
   }
 }
