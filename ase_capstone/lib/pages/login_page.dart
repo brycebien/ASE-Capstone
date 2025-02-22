7598ccb608993b559946fe92eb6d6af75f727afb
brycebien <brycebien@gmail.com>
2025-02-22 14:50:13 -0500

    created text fields for login page

--- ase_capstone/lib/pages/login_page.dart
+++ ase_capstone/lib/pages/login_page.dart
@@ -11,24 +11,74 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[300],
       appBar: AppBar(
-        title: const Text('Login'),
+        title: const Text('Campus Compass'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Welcome to the Login Page!',
-            ),
-            ElevatedButton(
-              onPressed: () {
-                // Navigate to the map page when the button is pressed
-                Navigator.pushNamed(context, '/map');
-              },
-              child: const Text('Go to Map Page'),
-            ),
-          ],
+      // Safe area to avoid notches and status bar
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 100),
+              // Campus Compass Logo
+              Icon(
+                Icons.account_circle,
+                size: 100,
+              ),
+              // Welcome Text
+              const Text(
+                'Welcome to the Login Page!',
+                style: TextStyle(
+                  fontSize: 24,
+                ),
+              ),
+              SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                // Username Text Field
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Username',
+                    // border colors
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                // Password Text Field
+                child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    // border colors
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey),
+                    ),
+                  ),
+                ),
+              ),
+              // sign in button
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  // Navigate to the map page when the button is pressed
+                  Navigator.pushNamed(context, '/map');
+                },
+                child: const Text('Sign In'),
+              ),
+            ],
+          ),
         ),
       ),
     );
