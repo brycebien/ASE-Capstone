68fec600189696dd78dec6cd5f310bd3a0a8581b
brycebien <brycebien@gmail.com>
2025-02-23 20:51:52 -0500

    added a dark theme

--- ase_capstone/lib/themes/dark_theme.dart
+++ ase_capstone/lib/themes/dark_theme.dart
@@ -2,4 +2,32 @@ import 'package:flutter/material.dart';
 
 final ThemeData darkTheme = ThemeData(
   brightness: Brightness.dark,
+
+  // app bar theme
+  appBarTheme: const AppBarTheme(
+    backgroundColor: Color.fromARGB(255, 54, 54, 54),
+
+    // items on app bar like settings or logout
+    foregroundColor: Color.fromARGB(255, 54, 54, 54),
+
+    // title on app bar
+    titleTextStyle: TextStyle(
+      fontSize: 26,
+      fontWeight: FontWeight.bold,
+      color: Color.fromARGB(255, 54, 54, 54),
+    ),
+  ),
+  colorScheme: ColorScheme.dark(
+    // main background color
+    surface: const Color.fromARGB(255, 54, 54, 54),
+
+    // primary color (app bar, buttons)
+    primary: const Color.fromARGB(255, 248, 120, 81),
+
+    // hint text (such as forgot password)
+    secondary: Colors.grey[600]!,
+
+    // Things like button text (text on primary color)
+    tertiary: const Color.fromARGB(255, 54, 54, 54),
+  ),
 );
