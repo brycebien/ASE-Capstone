574cef0492f0b583d88633840b4a2265d9eaa976
brycebien <brycebien@gmail.com>
2025-02-23 20:30:46 -0500

    created app bar theme for light mode

--- ase_capstone/lib/themes/light_theme.dart
+++ ase_capstone/lib/themes/light_theme.dart
@@ -2,6 +2,21 @@ import 'package:flutter/material.dart';
 
 final ThemeData lightTheme = ThemeData(
     brightness: Brightness.light,
+
+    // app bar theme
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Color.fromARGB(255, 248, 120, 81),
+
+      // items on app bar like settings or logout
+      foregroundColor: Color.fromARGB(255, 54, 54, 54),
+
+      // title on app bar
+      titleTextStyle: TextStyle(
+        fontSize: 26,
+        fontWeight: FontWeight.bold,
+        color: Color.fromARGB(255, 54, 54, 54),
+      ),
+    ),
     colorScheme: ColorScheme.light(
       // main background color
       surface: const Color.fromARGB(255, 245, 184, 165),
