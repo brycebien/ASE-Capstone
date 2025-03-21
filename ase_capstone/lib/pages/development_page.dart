59f9e4bbd406a2a76f7e15424c94aab9732a8332
brycebien <brycebien@gmail.com>
2025-03-21 17:58:40 -0400

    more todo implementation

--- ase_capstone/lib/pages/development_page.dart
+++ ase_capstone/lib/pages/development_page.dart
@@ -90,7 +90,9 @@ class _DevelopmentPageState extends State<DevelopmentPage> {
                                *    etc.
                                * 
                                * Allow user to edit university data such as buildings, address, resources, etc.
-                               * 
+                               * Allow user to add new buildings, resources, etc.
+                               * Allow user to delete buildings, resources, etc.
+                               * Allow user to add events
                                */
                             },
                           ),
