62ca7b86f461399bcbafbd28bcac56e7ce70c3f0
Andrey <drey.owen@gmail.com>
2025-04-06 18:09:20 -0400

    Bug Fixes

--- ase_capstone/lib/pages/profile_page.dart
+++ ase_capstone/lib/pages/profile_page.dart
@@ -86,7 +86,8 @@ class _ProfilePageState extends State<ProfilePage> {
         TextEditingController(text: user?.displayName);
     TextEditingController emailController =
         TextEditingController(text: user?.email);
-    TextEditingController usernameController = TextEditingController();
+    TextEditingController usernameController =
+        TextEditingController(text: userData['username']);
 
     showDialog(
       context: context,
